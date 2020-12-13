ede1d01a788977312f209c8eb11165a174a5ee67
Joel Caballero <joel.caballero@grupoleitz.com>
2020-12-12 23:53:20 -0600

    sintactico y lexico al cien

---
+++ Proyecto/Sintactico/b/CMakeFiles/LEXICO.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LEXICO.dir/Sintactico.cpp.o"
+  "CMakeFiles/LEXICO.dir/lexer.cpp.o"
+  "CMakeFiles/LEXICO.dir/main.cpp.o"
+  "LEXICO"
+  "LEXICO.pdb"
+  "lexer.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LEXICO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
